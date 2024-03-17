3826443d0f15eec8076eff5721c6556587956491
datamucho <datamucho3@gmail.com>
2024-03-17 21:02:10 +0100

    test audio

--- src/services/hardware/hw.service.ts
+++ src/services/hardware/hw.service.ts
@@ -115,8 +115,9 @@ class boxService extends serviceFactory<Document> {
       return next(new AppError("No book found with that ID", 404));
     }
 
-    const audio =
-      req.protocol + "://" + req.get("host") + "/request-audio/" + book.audio;
+    // const audio =
+    //   req.protocol + "://" + req.get("host") + "/request-audio/" + book.audio;
+    const audio = "http://stream.srg-ssr.ch/m/rsj/mp3_128";
 
     const volume = box.volume;
 
